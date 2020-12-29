d6034c38d6c600ccaefd2c689320587e02829c2e
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2020-12-29 12:46:59 +0600

    quiz 2 Test version

---
+++ Assignments/Theory/Quiz2/app/src/main/java/task/cse486/test/MainActivity.java
@@ -0,0 +1,14 @@
+package task.cse486.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
