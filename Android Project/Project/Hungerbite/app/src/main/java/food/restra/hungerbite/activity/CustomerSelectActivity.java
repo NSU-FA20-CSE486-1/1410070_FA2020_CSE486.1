2482f1497526c05bd27577f49b4842c2082b72b2
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2020-12-30 22:09:12 +0600

    user type select page design completed

---
+++ Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/activity/CustomerSelectActivity.java
@@ -0,0 +1,16 @@
+package food.restra.hungerbite.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import food.restra.hungerbite.R;
+
+public class CustomerSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_select);
+    }
+}
