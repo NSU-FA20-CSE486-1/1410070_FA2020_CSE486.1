6dbc7acd5888e4e4cb55f37d2c121c397220669a
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-01-29 00:39:57 +0600

    added jetpack bottom navigation in customer home page

--- Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/CustomerSelectActivity.java
+++ Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/CustomerSelectActivity.java
@@ -2,15 +2,34 @@ package food.restra.hungerbite.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import food.restra.hungerbite.R;
+import food.restra.hungerbite.customer.home.activity.CustomerHomeActivity;
 
 public class CustomerSelectActivity extends AppCompatActivity {
-
+    LinearLayout layoutCustomer, layoutChef, layoutDeliveryMan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_select);
+        initView();
+    }
+
+    public void initView(){
+        layoutCustomer = findViewById(R.id.llCustomer);
+        layoutChef = findViewById(R.id.llChef);
+        layoutDeliveryMan = findViewById(R.id.llDeliveryMan);
+
+        layoutCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerSelectActivity.this, CustomerHomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/OtpVerificationActivity.java
+++ Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/OtpVerificationActivity.java
@@ -2,15 +2,28 @@ package food.restra.hungerbite.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import food.restra.hungerbite.R;
 
 public class OtpVerificationActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verification);
+        initView();
+    }
+
+    public void initView(){
+        button = findViewById(R.id.btVerifyOtp);
+
+        button.setOnClickListener(view -> {
+            Intent intent =  new Intent(OtpVerificationActivity.this, CustomerSelectActivity.class);
+            startActivity(intent);
+        });
     }
 }
