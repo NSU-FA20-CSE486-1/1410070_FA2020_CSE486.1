966384f665e0a64b2e939b4fa6e25db5a7045217
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-02-02 02:12:45 +0600

    login system added with firebase

--- Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/LoginActivity.java
+++ Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/LoginActivity.java
@@ -1,25 +1,92 @@
 package food.restra.hungerbite.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import food.restra.hungerbite.R;
+import food.restra.hungerbite.customer.feed.activity.CustomerHomeActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button = findViewById(R.id.btSendOtp);
+        Button buttonLogin = findViewById(R.id.btLogin);
+        Button buttonSignup = findViewById(R.id.btSignup);
+        EditText editText = findViewById(R.id.etEmail);
+        EditText passwordText = findViewById(R.id.etPassword);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
-        button.setOnClickListener(view -> {
-            Intent intent = new Intent(LoginActivity.this, OtpVerificationActivity.class);
-            startActivity(intent);
+        buttonSignup.setOnClickListener(view -> {
+            mAuth.createUserWithEmailAndPassword(editText.getText().toString(), passwordText.getText().toString())
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, CustomerSelectActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
         });
+
+        buttonLogin.setOnClickListener(view -> mAuth.signInWithEmailAndPassword(editText.getText().toString(), passwordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, CustomerHomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                }));
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(LoginActivity.this, CustomerSelectActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/SplashActivity.java
+++ Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/login/SplashActivity.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import food.restra.hungerbite.R;
+import food.restra.hungerbite.customer.feed.activity.CustomerHomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    private static int TIME_OUT = 4000; //Time to launch the another activity
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(() -> {
-            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(i);
-            finish();
-        }, TIME_OUT);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(SplashActivity.this, CustomerHomeActivity.class);
+            startActivity(intent);
+        }else{
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
