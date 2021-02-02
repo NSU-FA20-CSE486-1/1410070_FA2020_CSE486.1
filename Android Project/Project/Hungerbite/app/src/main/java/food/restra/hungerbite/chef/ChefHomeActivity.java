9eaffa34317fcec1d69119a233dc52121cfd7573
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-02-02 19:05:27 +0600

    chef navigation system added

--- Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/chef/ChefHomeActivity.java
+++ Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/chef/ChefHomeActivity.java
@@ -2,6 +2,9 @@ package food.restra.hungerbite.chef;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import food.restra.hungerbite.R;
@@ -21,6 +25,9 @@ public class ChefHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_home);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
         mAuth = FirebaseAuth.getInstance();
     }
 
