00e61fd169299394ffaf39ac3d8e29aa1cb920dd
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-02-03 00:22:32 +0600

    product detail activity design implemented

---
+++ Android Project/Project/Hungerbite/app/src/main/java/food/restra/hungerbite/customer/product_detail/ProductDetailActivity.java
@@ -0,0 +1,16 @@
+package food.restra.hungerbite.customer.product_detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import food.restra.hungerbite.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
